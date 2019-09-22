6d4b47481aa5eb0c97ed7f00378d590c7a63dad2
Demomaker <mikempatate@hotmail.com>
2019-09-22 00:11:04 -0400

    First commit

---
+++ SFMLMaker/Game.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML.Graphics;
+
+namespace SFMLMaker
+{
+    class Game
+    {
+        internal void Inputs()
+        {
+            //
+            throw new NotImplementedException();
+        }
+
+        internal void Draw(RenderWindow renderWindow)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SFMLMaker/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML;
+using SFML.Graphics;
+
+namespace SFMLMaker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game();
+            RenderWindow renderWindow = new RenderWindow(SFML.Window.VideoMode.DesktopMode, "Jérémie Bertrand", SFML.Window.Styles.Titlebar);
+            while(renderWindow.IsOpen)
+            {
+                game.Inputs();
+                game.Update();
+                game.Draw(renderWindow);
+            }
+        }
+    }
+}
